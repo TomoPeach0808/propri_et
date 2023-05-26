0ccd07c3a283a704246e5814a45d25afa99966b1
TomoPeach0808 <131196810+TomoPeach0808@users.noreply.github.com>
2023-05-26 16:44:50 -0500

    Update Test_Roboclaw.m

--- Test_Roboclaw.m
+++ Test_Roboclaw.m
@@ -1,44 +1,32 @@
 import Roboclaw
 
-my_roboclaw = Roboclaw("/dev/tty.usbmodem14101", 14101);
+clear
+% comport = "/dev/tty.usbmodem14101"; % Tomomi's macbook
+comport = "COM4";  %lab computer; go on device manager to check
+
+my_roboclaw = Roboclaw(comport, 9600);
 [my_roboclaw, result] = my_roboclaw.Open(); %Establish connection
 
 if result == 1
     disp('Connection Established')
+    
+    % address = dec2hex(128);
+    address = 128;
+    speed = 10;
+
+    display(my_roboclaw)
+
+    my_port = my_roboclaw.port;
+
+    display(my_port)
+
+    data = read(my_port, 1,"uint8");
+
+    my_roboclaw.ForwardM1(address, speed);
+    
+
 else
     disp('Unable to establish connection')  
 end
 
-my_port = my_roboclaw.port;
-
-
-writeline(my_port,"*IDN?")
-my_port.NumBytesAvailable
-
-% % fopen(my_port);
-% 
-% disp('Serial Port Monitor Started. Press Ctrl+C to stop.');
-% 
-% try
-%     while count > 100000
-%         if my_port.NumBytesAvailable > 0
-%             data = fread(my_port, my_port.NumBytesAvailable);
-%             disp(['Received data: ', char(data')]);
-%             count = count + 1;
-%         end
-%     end
-% catch ME
-%     disp('Serial Port Monitor Stopped.');
-% end
-% 
-% % Close the serial port
-% clear my_port;
-
-% address = dec2hex(128);
-% address = 128;
-% speed = 5;
-% 
-% 
-% my_roboclaw.ForwardM1(address, speed);
-% 
-% clear my_roboclaw % Disconnects
+clear my_roboclaw % Disconnects
